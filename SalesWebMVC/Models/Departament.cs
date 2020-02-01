242d4daa9f4034c4aa12159f58d66964518198fc
Kaue Magid <magidsalem96@gmail.com>
2020-02-01 12:01:10 -0300

    create other entities and sync with DB

--- SalesWebMVC/Models/Departament.cs
+++ SalesWebMVC/Models/Departament.cs
@@ -1,9 +1,33 @@
-﻿namespace SalesWebMVC.Models
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SalesWebMVC.Models
 {
     public class Departament
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Saller> Sallers { get; set; } = new List<Saller>();
 
+        public Departament()
+        {
+        }
+
+        public Departament(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSaller(Saller saller)
+        {
+            Sallers.Add(saller);
+        }
+
+        public double TotalDepartament(DateTime initial, DateTime final)
+        {
+            return Sallers.Sum(saller => saller.TotalSales(initial,final));          
+        }
     }
 }
